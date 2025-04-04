9d6094f5ce42b8c1dd72453797b7544f761a9e22
PriyanshuSinghCo <priyanshu2002krsingh@gmail.com>
2025-04-05 00:38:25 +0530

    react-router

--- currency/src/hooks/useCurrencyinfo.js
+++ currency/src/hooks/useCurrencyinfo.js
@@ -3,7 +3,9 @@ import {useEffect, useState} from "react";
 function useCurrencyInfo(currency) {
     const [data, setData] = useState({})
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+         let url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+
+        fetch(url)
         .then((res) => res.json())
         .then((res) => setData(res[currency]))
         console.log(data);
